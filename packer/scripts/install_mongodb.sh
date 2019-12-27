1c966162f873622111f96fdb52de8363471ebd3d
Pavel Batsev <pbatsev@sportmaster.ru>
2019-12-27 08:44:00 +0300

    devops-2019-11. packer-base.git mv old files to config-scripts. Add files for hw5 to packer+config-scripts. Add files to.girignore. Edit readme.md

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# Install MongoDB
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
+apt --assume-yes update
+apt --assume-yes install mongodb-org
+systemctl enable mongod
