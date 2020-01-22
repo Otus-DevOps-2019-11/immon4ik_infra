66fc081995757fac4db61df2bdb11cf63b7933da
Pavel Batsev <pbatsev@sportmaster.ru>
2020-01-22 09:25:16 +0300

    devops-2019-11. terraform-1. Edit main.tf, .github\PULL_REQUEST_TEMPLATE.md.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
   # Версия terraform
-  required_version = "0.12.8"
+  required_version = "~>0.12.8"
 }
 
 provider "google" {
-  version = "2.15.0"
+  version = "~>2.15"
   project = var.project
   region  = var.region
 }
