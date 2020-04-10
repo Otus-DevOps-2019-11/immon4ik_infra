ae79c8eb628ba7d0f5ad990dbd3f9d860e1d453e
Pavel Batsev <PBatsev@sportmaster.ru>
2020-04-10 14:48:35 +0300

    devops-2019-11. ansible-3.  Finalization of improvements.

--- play-travis/ansible-3/run_tests_in_docker.sh
+++ play-travis/ansible-3/run_tests_in_docker.sh
@@ -5,21 +5,6 @@ HOMEWORK_RUN=./play-travis/ansible-3/tests/run_tests.sh
 # Используется пользователь не immon4ik, а appuser, т.к. в контейнере создан только пользователь appuser.
 if [ -f $HOMEWORK_RUN ]; then
 	echo "Run tests (linters, validators)"
-	# sudo pip install --upgrade pip
-	# sudo pip install -r ansible/requirements.txt
-	# ansible --version
-	# ansible-lint --version
-	# terraform --version
-	# packer --version
-	# find . -type f -name '*.tf' -exec terraform fmt {} \;
-	# cp terraform/prod/terraform.tfvars.example terraform/prod/terraform.tfvars
-	# cp terraform/stage/terraform.tfvars.example terraform/stage/terraform.tfvars
-	# terraform get terraform/stage
-	# find terraform/stage -type f -name '*.tf' -exec tflint --ignore-module=SweetOps/storage-bucket/google {} \;
-	# terraform get terraform/prod
-	# find terraform/prod -type f -name '*.tf' -exec tflint --ignore-module=SweetOps/storage-bucket/google {} \;
-	# find . -type f -name '*.json' -exec packer validate -var-file=packer/variables.json.example {} \;
-	# find . -type f -name '*.yml' -exec ansible-lint {} \;
 	docker exec -e USER=appuser hw-test $HOMEWORK_RUN
 else
 	echo "We don't have any tests"
--- play-travis/ansible-3/tests/run_tests.sh
+++ play-travis/ansible-3/tests/run_tests.sh
@@ -7,12 +7,11 @@ touch ~/.ssh/appuser.pub ~/.ssh/appuser
 
 # Установка софта для окружения и ролей из ansible-galaxy, описанные в requirements.yml.
 # sudo pip install -r ansible/requirements.txt
-pwd && ls
 ansible --version
 ansible-lint --version
 terraform --version
 packer --version
-# cd ansible && ansible-galaxy install -r environments/stage/requirements.yml && cd ..
+cd ansible && ansible-galaxy install -r environments/stage/requirements.yml && cd ..
 
 # Запуск inspec.
 inspec exec play-travis/ansible-3/tests/
