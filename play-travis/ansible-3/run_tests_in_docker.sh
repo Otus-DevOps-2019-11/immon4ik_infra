ea79e7fab148e9712c280d21c1a78bc7c13e27c2
Pavel Batsev <PBatsev@sportmaster.ru>
2020-04-10 12:30:47 +0300

    devops-2019-11. ansible-3.  Fix  run_*.sh scripts.

--- play-travis/ansible-3/run_tests_in_docker.sh
+++ play-travis/ansible-3/run_tests_in_docker.sh
@@ -1,22 +1,5 @@
-#!/bin/bash
-GROUP=2019-11
-BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-HOMEWORK_RUN=./otus-homeworks/homeworks/$BRANCH/run.sh
-REPO=https://github.com/express42/otus-homeworks.git
-DOCKER_IMAGE=express42/otus-homeworks:0.7.1
+#!/usr/bin/env bash
 HOMEWORK_RUN=./play-travis/ansible-3/tests/run_tests.sh
-
-echo GROUP:$GROUP
-
-if [ "$BRANCH" == "" ]; then
-	echo "We don't have tests for master branch"
-	exit 0
-fi
-
-echo HOMEWORK:$BRANCH
-
-echo "Clone repository with tests"
-git clone -b $GROUP --single-branch $REPO
 
 # Запуск скрипта преподготовки среды тестирования и выполнения тестов из подготовленных сценариев.
 # Используется пользователь не immon4ik, а appuser, т.к. в контейнере создан только пользователь appuser.
--- play-travis/ansible-3/tests/run_tests.sh
+++ play-travis/ansible-3/tests/run_tests.sh
@@ -6,6 +6,11 @@ set -e
 touch ~/.ssh/appuser.pub ~/.ssh/appuser
 
 # Установка ролей из ansible-galaxy, описанные в requirements.yml.
+pip install -r ansible/requirements.txt
+- ansible --version
+- ansible-lint --version
+- terraform --version
+- packer --version
 cd ansible && ansible-galaxy install -r environments/stage/requirements.yml && cd ..
 
 # Запуск inspec.
